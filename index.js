c6fe6b76d9bb46b1a40599edd22692767ce0d7e6
GabrielK1410 <kaunanggabriel0@gmail.com>
2025-11-11 19:07:26 +0800

    signup & nav

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ import {name as appName} from './app.json';
 import App from './App';
 import LandingPage from './src/pages/LandingPage';
 import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
 
 AppRegistry.registerComponent(appName, () => App);
